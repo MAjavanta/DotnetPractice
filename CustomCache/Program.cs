16cf419de4e5b0c44d27d852c0c5711beda02d61
MAjavanta <ma.javanta@gmail.com>
2026-03-01 12:26:22 +0000

    Create starting point for new custom cache project

---
+++ CustomCache/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Hello, World!");
+
+public class Cache<Tkey, TValue> where Tkey : notnull
+{
+    private Dictionary<Tkey, TValue> _cache = [];
+
+    public bool Contains(Tkey key) => _cache.ContainsKey(key);
+
+    public void Store(Tkey key, TValue value) => _cache[key] = value;
+
+    public TValue GetByKey(Tkey key) => _cache[key];
+
+}
