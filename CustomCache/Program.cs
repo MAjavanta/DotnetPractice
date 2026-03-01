09999f2cdfa2cf413c349e207eb4c1644e97dc27
MAjavanta <ma.javanta@gmail.com>
2026-03-01 13:35:48 +0000

    Implemented usage of cache using the decorator design pattern

--- CustomCache/Program.cs
+++ CustomCache/Program.cs
@@ -1,4 +1,49 @@
-﻿Console.WriteLine("Hello, World!");
+﻿IDataDownloader dataDownloader = new CachingDataDownloader(
+                                        new Cache<string, string>(),
+                                        new SlowDataDownloader());
+
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+
+Console.ReadKey();
+
+public interface IDataDownloader
+{
+    string DownloadData(string resourceId);
+}
+
+public class SlowDataDownloader : IDataDownloader
+{
+    public string DownloadData(string resourceId)
+    {
+        Thread.Sleep(1000);
+        return $"Some data for {resourceId}";
+    }
+}
+
+public class CachingDataDownloader(
+                    Cache<string, string> cache, SlowDataDownloader dataDownloader
+            ) : IDataDownloader
+{
+    private readonly Cache<string, string> _cache = cache;
+    private readonly SlowDataDownloader _dataDownloader = dataDownloader;
+
+    public string DownloadData(string resourceId)
+    {
+        if (_cache.Contains(resourceId))
+        {
+            return _cache.GetByKey(resourceId);
+        }
+        var result = _dataDownloader.DownloadData(resourceId);
+        _cache.Store(resourceId, result);
+        return result;
+    }
+}
 
 public class Cache<Tkey, TValue> where Tkey : notnull
 {
